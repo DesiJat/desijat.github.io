6b764e75d6563de8a160e870d45329b35ef39546
apple <apple@Apples-MacBook-Pro.local>
2026-09-02 16:37:21 +0530

    shift book into files folder

--- books/books.js
+++ books/books.js
@@ -1,11 +1,11 @@
 const files = [
-    { name: "Shreemad Bhagwad Geeta", path: "shreemadBhagwadGeeta.md" },
-    { name: "Patanjali Yoga Sutras", path: "patanjaliYogsutr.md" },
-    { name: "Vedic Time Table", path: "timeTable.md" },
-    { name: "Vigyan Bhairav Tantra", path: "vigyanBharvTantar.md" },
-    { name: "Maharishi Panini Ashtadhyayi", path: "maharishiPaniniAshtadhyayi.md" },
-    { name: "Vedic Mathematics", path: "vedicMath.md" },
-    { name: "72-Karikas", path: "72-Karikas.md" },
-    { name: "yoga-sutra-Google", path: "yoga-sutra-Google.md" },
-    { name: "YogaSutrasStudy", path: "YogaSutrasStudy.md" }
+    { name: "Patanjali Yoga Sutras", path: "files/patanjaliYogsutr.md" },
+    { name: "Shreemad Bhagwad Geeta", path: "files/shreemadBhagwadGeeta.md" },
+    { name: "Vedic Time Table", path: "files/timeTable.md" },
+    { name: "Vigyan Bhairav Tantra", path: "files/vigyanBharvTantar.md" },
+    { name: "Maharishi Panini Ashtadhyayi", path: "files/maharishiPaniniAshtadhyayi.md" },
+    { name: "Vedic Mathematics", path: "files/vedicMath.md" },
+    { name: "72-Karikas", path: "files/72-Karikas.md" },
+    { name: "yoga-sutra-Google", path: "files/yoga-sutra-Google.md" },
+    { name: "YogaSutrasStudy", path: "files/YogaSutrasStudy.md" }
 ];
