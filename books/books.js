9e550f92d51663bcec2de7e054c9c35f5cfca645
apple <apple@Apples-MacBook-Pro.local>
2026-09-02 16:29:32 +0530

    files set in books.js

---
+++ books/books.js
@@ -0,0 +1,11 @@
+const files = [
+    { name: "Shreemad Bhagwad Geeta", path: "shreemadBhagwadGeeta.md" },
+    { name: "Patanjali Yoga Sutras", path: "patanjaliYogsutr.md" },
+    { name: "Vedic Time Table", path: "timeTable.md" },
+    { name: "Vigyan Bhairav Tantra", path: "vigyanBharvTantar.md" },
+    { name: "Maharishi Panini Ashtadhyayi", path: "maharishiPaniniAshtadhyayi.md" },
+    { name: "Vedic Mathematics", path: "vedicMath.md" },
+    { name: "72-Karikas", path: "72-Karikas.md" },
+    { name: "yoga-sutra-Google", path: "yoga-sutra-Google.md" },
+    { name: "YogaSutrasStudy", path: "YogaSutrasStudy.md" }
+];
